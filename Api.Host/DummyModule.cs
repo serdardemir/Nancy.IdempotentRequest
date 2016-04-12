15d15bf4a578c9539954d91b77d3dafd89cb69f1
Serdar Demir <serdardemir@outlook.com>
2016-04-12 23:21:08 +0300

    Etag Generator and caching added

--- Api.Host/DummyModule.cs
+++ Api.Host/DummyModule.cs
@@ -1,28 +1,58 @@
 ﻿using Nancy;
 using System;
 using System.Collections.Generic;
+using Nancy.ModelBinding;
 using System.Linq;
 using System.Web;
+using Nancy.IdempotentRequest.Extensions;
+using Nancy.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Api.Host
 {
     public class DummyModule : NancyModule
     {
         public DummyModule()
+        //: base("api")
         {
             {
                 Put["/updateBlog"] = p =>
                 {
-                    //this.Context.EnableOutputCache(30);
+                    this.Context.EnableOutputCache(300);
+                    
+                    dynamic request = JsonConvert.DeserializeObject(Request.Body.AsString());
 
+                    var blog = new Blog
+                    {
+                        blogId = request.Value<int>("blogId"),
+                        title = request.Value<string>("title"),
+                        addedDate = request.Value<DateTime>("addedDate"),
+                        tag = request.Value<string>("tag"),
+                    };
 
-                    //Blog blog = this.Bind<Blog>();
-                    //BlogFixture.Blogs.Add(blog);
-                    //return BlogFixture.Blogs.Count;
                     return null;
                 };
 
+                Get["/"] = p =>
+                {
+                    return "";
+                };
             }
+        }
+
+        public class Blog
+        {
+            public int blogId { get; set; }
+
+            public string title { get; set; }
+
+            public string body { get; set; }
+
+            public string tag { get; set; }
+
+            public DateTime addedDate { get; set; }
         }
     }
 }
--- Nancy.IdempotentRequest/Helper/EntityFilterHelper.cs
+++ Nancy.IdempotentRequest/Helper/EntityFilterHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Nancy.IdempotentRequest
+namespace Nancy.IdempotentRequest.Helper
 {
     public static class EntityFilterHelper
     {
